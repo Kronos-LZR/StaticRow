8b2427c903af5173eab13fca2d37bdc0f6bdcadc
Lazaro <lazaro@negocie.online>
2018-03-14 15:11:01 -0300

    removendo comentarios chatos

--- StaticRow/src/staticrow/IRow.java
+++ StaticRow/src/staticrow/IRow.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package staticrow;
 
-/**
- *
- * @author lazar
- */
 public interface IRow {
     void enqueue(int value);
     int dequeue();
